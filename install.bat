3aaf6741bb2a1989413716fffb4cb37be7685a5a
Igor Malinovskiy <u.glide@gmail.com>
2013-09-16 16:31:41 +0300

    Updated unicode installer

--- install.bat
+++ install.bat
@@ -4,19 +4,13 @@ if exist "C:\Program Files (x86)\NSIS\NSIS.exe"  (
 	echo NSIS installed
 	copy /Y "Ansi\\Include\\*.nsh"  "C:\Program Files (x86)\\NSIS\\Include\\"
 	copy /Y "Ansi\\Plugins\\*.dll"  "C:\Program Files (x86)\\NSIS\\Plugins\\"
-
-
-	if exist "C:\Program Files (x86)\NSIS\Unicode\NSIS.exe" (
-		echo NSIS Unicode installed
-		copy /Y "Ansi\\Include\\*.nsh"  "C:\\Program Files (x86)\\NSIS\Unicode\\Include\\"
-		copy /Y "Ansi\\Plugins\\*.dll"  "C:\\Program Files (x86)\\NSIS\\Unicode\\Plugins\\"
-		copy /Y "Unicode\\Include\\*.nsh"  "C:\\Program Files (x86)\\NSIS\\Unicode\\Include\\"
-		copy /Y "Unicode\\Plugins\\*.dll"  "C:\\Program Files (x86)\\NSIS\\Unicode\\Plugins\\"		
-
-	)
 	
-) else (
-	echo Install NSIS first.	
+)
+
+if exist "C:\Program Files (x86)\NSIS\Unicode\NSIS.exe" (
+	echo NSIS Unicode installed
+	copy /Y "Unicode\\Include\\*.nsh"  "C:\\Program Files (x86)\\NSIS\\Unicode\\Include\\"
+	copy /Y "Unicode\\Plugins\\*.dll"  "C:\\Program Files (x86)\\NSIS\\Unicode\\Plugins\\"		
 )
 
 
